e7da6cc3bd43cf7d6a15427d62b684e47e8576fc
Sitthata <aprils96950@gmail.com>
2025-03-21 15:43:05 +0700

    Enhance Playwright configuration and improve test structure. Added video recording and slow motion options for better debugging in `playwright.config.ts`. Refactored test cases in `customer-purchase-product.spec.ts` for improved readability and consistency, ensuring all test scenarios are clearly defined and properly formatted.

--- e2e/customer-purchase-product.spec.ts
+++ e2e/customer-purchase-product.spec.ts
@@ -1,52 +1,54 @@
-import test, { expect } from "@playwright/test";
+import test from "@playwright/test";
 import { ProductsPage } from "./pages/products-page";
 import { ProductDetailPage } from "./pages/product-detail-page";
 
 test.describe("Customer Purchase Product", () => {
-    test("Navigates to the products page and displays the correct title", async ({
-        page,
-      }) => {
-        const productsPage = new ProductsPage(page);
-        await productsPage.goToProductsPage();
-        await productsPage.displayProductsPageTitle();
-      });
-      
-      test("Displays the correct number of products in the product list", async ({
-        page,
-      }) => {
-        const productsPage = new ProductsPage(page);
-        await productsPage.goToProductsPage();
-        await productsPage.displayAllProducts(16);
-      });
+  test("Navigates to the products page and displays the correct title", async ({
+    page,
+  }) => {
+    const productsPage = new ProductsPage(page);
+    await productsPage.goToProductsPage();
+    await productsPage.displayProductsPageTitle();
+  });
 
-      test("Displays correct product details when a specific product is selected", async ({
-        page,
-      }) => {
-        const productDetailPage = new ProductDetailPage(page);
-        await productDetailPage.goToProductDetailPage();
-        await productDetailPage.displayProductTitle();
-        await productDetailPage.displayProductPrice();
-        await productDetailPage.displayProductImage();
-      });
+  test("Displays the correct number of products in the product list", async ({
+    page,
+  }) => {
+    const productsPage = new ProductsPage(page);
+    await productsPage.goToProductsPage();
+    await productsPage.displayAllProducts(16);
+  });
 
-      test("Successfully completes order with valid customer information", async ({
-        page,
-      }) => {
-        const productsDetailPage = new ProductDetailPage(page);
-        await productsDetailPage.goToProductDetailPage();
-        await productsDetailPage.fillValidCustomerInformation("First", "0875551234");
-        await productsDetailPage.clickConfirmOrderButton();
-        await productsDetailPage.displayThankYouMessage();
-      });
-      
-      test("Prevents order completion when customer information is missing", async ({
-        page,
-      }) => {
-        const productsDetailPage = new ProductDetailPage(page);
-        await productsDetailPage.goToProductDetailPage();
-        await productsDetailPage.fillInvalidCustomerInformation();
-        await productsDetailPage.clickConfirmOrderButton();
-        await productsDetailPage.notDisplayThankYouMessage();
-      });
-      
-})
+  test("Displays correct product details when a specific product is selected", async ({
+    page,
+  }) => {
+    const productDetailPage = new ProductDetailPage(page);
+    await productDetailPage.goToProductDetailPage();
+    await productDetailPage.displayProductTitle();
+    await productDetailPage.displayProductPrice();
+    await productDetailPage.displayProductImage();
+  });
+
+  test("Successfully completes order with valid customer information", async ({
+    page,
+  }) => {
+    const productsDetailPage = new ProductDetailPage(page);
+    await productsDetailPage.goToProductDetailPage();
+    await productsDetailPage.fillValidCustomerInformation(
+      "First",
+      "0875551234"
+    );
+    await productsDetailPage.clickConfirmOrderButton();
+    await productsDetailPage.displayThankYouMessage();
+  });
+
+  test("Prevents order completion when customer information is missing", async ({
+    page,
+  }) => {
+    const productsDetailPage = new ProductDetailPage(page);
+    await productsDetailPage.goToProductDetailPage();
+    await productsDetailPage.fillInvalidCustomerInformation();
+    await productsDetailPage.clickConfirmOrderButton();
+    await productsDetailPage.notDisplayThankYouMessage();
+  });
+});
